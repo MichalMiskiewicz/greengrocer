897c4367a1f03f4f5771b1a6d87ad8cd95ad6922
MichalMiskiewicz <miskiewiczmd@gmail.com>
2022-04-19 12:14:33 +0200

    Initial commit

---
+++ src/main/java/pl/miskiewiczmichal/greengrocerapi/GreengrocerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.miskiewiczmichal.greengrocerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengrocerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreengrocerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/miskiewiczmichal/greengrocerapi/GreengrocerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miskiewiczmichal.greengrocerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreengrocerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
